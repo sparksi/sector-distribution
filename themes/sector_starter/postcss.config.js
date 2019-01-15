efeb6f2a453f1f87ad39567a397ec45cbb704da1
Tom Grinsted <tom@sparksinteractive.co.nz>
2019-01-15 16:31:00 +1300

    Remove GULP SVG and replace with SVG POST tool 22221

--- themes/sector_starter/postcss.config.js
+++ themes/sector_starter/postcss.config.js
@@ -1,10 +1,13 @@
-module.exports = {
+module.exports = ctx => ({
   plugins: {
     autoprefixer: {
       browsers: [
         "last 2 version"
       ],
       grid : true
+    },
+    "postcss-inline-svg": {
+      "xmlns": true
     }
   }
-}
+})
