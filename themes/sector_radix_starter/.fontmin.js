088c6625c339313d450527a7b5e8ef65c8758a52
Gareth Poole <garethpoole@icloud.com>
2021-03-04 15:23:25 +1300

    Issue #3201521 by DanielVeza, gareth.poole: improved font generation pipeline

--- themes/sector_radix_starter/.fontmin.js
+++ themes/sector_radix_starter/.fontmin.js
@@ -1,10 +1,14 @@
 // convert ttf in src/fonts to all websafe font variations
 
 const Fontmin = require('fontmin');
+const fs = require('fs');
+const path = require('path');
+
+const OUT_DIR = `dist/fonts`
  
 const fontmin = new Fontmin()
     .src('src/fonts/*.ttf')
-    .dest('dist/fonts')
+    .dest(OUT_DIR)
     .use(Fontmin.ttf2woff({
         deflate: true
     }))
@@ -14,10 +18,22 @@ const fontmin = new Fontmin()
     .use(Fontmin.ttf2eot());
 
 
-fontmin.run(function (err, files) {
+fontmin.run(async (err, files) => {
     if (err) {
         throw err;
     }
      
-    //console.log(files);
+    const dist = files.map(file => {
+        const { history } = {...file};
+        return history.find(f => f.includes('/dist/'));
+    })
+
+    const woff2 = dist.filter(file => file.includes('woff2')).map(file => `${OUT_DIR}/${path.basename(file)}`)
+
+    if(woff2.length > 0) {
+        await fs.writeFile(`./${OUT_DIR}/preload-fonts.yaml`, `fonts:\n - ${woff2.join(`\n - `)}`, () => {
+            console.log(`🆕 written ${OUT_DIR}/preload-fonts.yaml for improved font preloading`)
+        });
+    }
+
 });
