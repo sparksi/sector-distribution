b346117e2682edbb95ac04bdcb7dcad0cdb5e142
Stewest <weststew@gmail.com>
2023-05-19 13:17:20 +1200

    fix(alert): syntax from 4 to 2 spaces

--- themes/sector_starter/src/components/alert/alert.ts
+++ themes/sector_starter/src/components/alert/alert.ts
@@ -1,23 +1,22 @@
-const root = document.querySelector('.canvas')
+const root = document.querySelector('.canvas');
 
 Drupal.behaviors.alert = {
-    attach: (context, settings) => {
+  attach: (context, settings) => {
+    const dialogs = context.querySelectorAll('.alert--dismissible');
 
-        const dialogs = context.querySelectorAll('.alert--dismissible');
-
-        /*dialogs.forEach((dialog: HTMLDialogElement) => {
+    /*dialogs.forEach((dialog: HTMLDialogElement) => {
             //dialog.showModal();
             dialog.show();
         });*/
 
-        dialogs.forEach((dialog: HTMLElement) => {
-            const close = dialog.querySelector('.alert__close');
-            if(close) {
-                close.addEventListener('click', (event: Event) => {
-                    //dialog.close();
-                    dialog.remove();
-                })
-            }
+    dialogs.forEach((dialog: HTMLElement) => {
+      const close = dialog.querySelector('.alert__close');
+      if (close) {
+        close.addEventListener('click', (event: Event) => {
+          //dialog.close();
+          dialog.remove();
         });
-    }    
+      }
+    });
+  },
 };
