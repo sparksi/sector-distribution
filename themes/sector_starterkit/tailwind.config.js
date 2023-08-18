f3f0d74872d60542a2ac3d815758d80650731e76
Gareth Poole <gareth@sparksinteractive.co.nz>
2023-08-18 14:50:32 +1200

    make sure starterkit compiles!

--- themes/sector_starterkit/tailwind.config.js
+++ themes/sector_starterkit/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   content: [
@@ -27,15 +29,24 @@ module.exports = {
         '3xl': '1920px',
         '4xl': '2560px',
       },
+      colors: {
+        primary: `var(--primary, ${colors.gray['500']})`,
+        alert: `var(--alert, ${colors.orange["800"]})`,
+        notice: `var(--alert, ${colors.amber["700"]})`,
+        highlight: `var(--alert, ${colors.amber["100"]})`,
+      },
       fontFamily: {
         custom: ['var(--custom-font)'],
-      }
+      },
+      animation: {
+        alert: 'fadeInUp .35s ease',
+      },
     },
   },
   plugins: [
     require('@tailwindcss/container-queries'),
     require('@tailwindcss/forms')({
-      strategy: 'class',
+      strategy: 'base',
     }),
     require('@tailwindcss/typography')({
       className: 'wysiwyg',
