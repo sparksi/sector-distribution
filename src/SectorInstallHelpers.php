01cc3401a57104af5f6c92513616c4d98077a97d
Gareth Poole <work@garethpoole.com>
2023-08-30 12:05:32 +1000

    changed namespace

--- src/SectorInstallHelpers.php
+++ src/SectorInstallHelpers.php
@@ -66,4 +66,38 @@ class SectorInstallHelpers implements ContainerInjectionInterface {
       $child_block->save();
     }
   }
+
+  // I wanted to put this in sector_custom_blocks module :-(
+  public static function setupCustomBlockQueue() {
+
+    $queue_id = 'homepage_promotion_cards';
+
+    // Load the entity queue.
+    $entity_queue = \Drupal::entityTypeManager()->getStorage('entity_queue')->load($queue_id);
+
+    if ($entity_queue) {
+        $block_ids = [
+            '58a7f20e-651c-4821-b5f8-0b469a5d0291',
+            '26441a99-98ec-4b96-a85b-81255116541b',
+            '4adddefb-9f1f-4ee5-af7d-a12b257ddf34'
+        ];
+
+        $subqueue = EntitySubqueue::load($queue_id);
+
+        foreach($block_ids as $block_id) {
+            $block_content = \Drupal::service('entity.repository')->loadEntityByUuid('block_content', $block_id);
+
+            if ($block_content) {
+                $subqueue->addItem($block_content);
+                $subqueue->save();
+            }
+            else {
+                \Drupal::logger('sector_promo_magnet')->error('Entity not found.');
+            }
+        }
+    }
+    else {
+        \Drupal::logger('sector_promo_magnet')->error('Entity queue not found.');
+    }
+  }
 }
